1deb81993ed06d22a30816d4a832032867717ae5
ZbrMa <zbranek.m@gmail.com>
2024-12-07 20:13:08 +0100

    backend url changed

--- frontend/src/api/apiSlice.ts
+++ frontend/src/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.BAKCNED_URL }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BAKCNED_URL }),
     endpoints: builder => ({}),
     tagTypes:['events', 'userEvents','notificationOut','notificationIn','places'],
     keepUnusedDataFor: 5,
